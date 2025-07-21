816923a9d0a89c73a9b16acc0e494f8e99df638f
Astatke Belay <starkey.belay@gov.ab.ca>
2025-07-20 18:57:47 -0600

    removing the unsupported options from the database configuration.

--- config/db.js
+++ config/db.js
@@ -14,9 +14,8 @@ const connectDB = async () => {
       const conn = await mongoose.connect(process.env.MONGODB, {
         maxPoolSize: 10, // Maintain up to 10 socket connections
         serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-        bufferCommands: false, // Disable mongoose buffering
-        bufferMaxEntries: 0 // Disable mongoose buffering
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds
+        bufferCommands: false // Disable mongoose buffering
       });
       isConnected = true;
       console.log(`MongoDB Connected: ${conn.connection.host}`);
